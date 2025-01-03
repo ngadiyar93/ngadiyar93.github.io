e8e6e70cbccc86b8683c2057a42458d9c298412f
ngadiyar93 <43225038+ngadiyar93@users.noreply.github.com>
2025-01-03 14:33:06 +0000

    Deploying to gh-pages from @ ngadiyar93/ngadiyar93.github.io@6d755a9765f48c1bfc78ea38b6487366cd46fc06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,7 +397,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-our-paper-development-of-solid-synchronous-reluctance-rotors-with-multi-material-additive-manufacturing-is-accepted-in-the-ieee-transactions-on-industry-applications-and-available-on-ieee-xplore",
+          section: "News",},{id: "news-our-paper-development-of-solid-synchronous-reluctance-rotors-with-multi-material-additive-manufacturing-is-accepted-for-publication-in-the-ieee-transactions-on-industry-applications-and-available-on-ieee-xplore",
           title: 'Our paper Development of Solid Synchronous Reluctance Rotors With Multi-Material Additive Manufacturing is...',
           description: "",
           section: "News",},{id: "projects-project-1",
