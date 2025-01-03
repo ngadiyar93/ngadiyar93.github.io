01022bf1a225e24554416c84643624630157dfcf
ngadiyar93 <43225038+ngadiyar93@users.noreply.github.com>
2025-01-03 23:05:51 +0000

    Deploying to gh-pages from @ ngadiyar93/ngadiyar93.github.io@24807e16ca0dda6de6070fc56ff1ef3b5055de79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-multi-harmonic-electric-machines",
+          title: 'Multi-harmonic Electric Machines',
+          description: "Technique to unlock additional degrees of freedom in e-Machines",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
