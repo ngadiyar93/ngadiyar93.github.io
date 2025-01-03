b08422dd2c7fe84f69e9dbf94d54645f0caa20d9
ngadiyar93 <43225038+ngadiyar93@users.noreply.github.com>
2025-01-03 01:56:30 +0000

    Deploying to gh-pages from @ ngadiyar93/ngadiyar93.github.io@a404cf531b0ea00c47bc682a909bcd509cc58f89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/sample-posts/2015/03/15/formatting-and-links.html";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-development-of-solid-synchronous-reluctance-rotors-with-multi-material-additive-manufacturing-is-accepted-in-the-ieee-transactions-on-industry-applications-and-available-on-ieee-xplore",
+          title: 'Our paper Development of Solid Synchronous Reluctance Rotors With Multi-Material Additive Manufacturing is...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
